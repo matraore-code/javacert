b515f4c49646f60cf28a185a4167db01945bd41e
matraore-code <mamadou.moctartra@gmail.com>
2023-02-20 16:11:47 +0100

    Exercise

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        /*
+        Exercise 1 : Create a class called Compile.java with the following code
+        package com.amigoscode;
+            public class Exercise {
+            public static void main(String[] args) {
+            System.out.println("I can compile and run from the terminal. Hooray 🎉");
+            }
+           }
+       Solution : Copy the line "System.out.println("I can compile and run from the terminal. Hooray 🎉");" then run on IntelliJ
+        */
 
-        System.out.println("Hello world test");
-    }
+        /*
+        Exercise 2 : Make sure when the following code runs it only prints I love cats. Don't delete the any code.
+              package com.amigoscode;
+
+                public class Exercise {
+                public static void main(String[] args) {
+                System.out.println("I love cats");
+                System.out.println("I hate cats");
+            }
+            }
+            Solution : copy the code below and then comment the second line with //
+             System.out.println("I love cats");
+            System.out.println("I hate cats");
+        */
+
+
+        }
 }
