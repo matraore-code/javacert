702f4570097f744ae61e09f3b23fa6f5fbd0fed4
matraore-code <mamadou.moctartra@gmail.com>
2023-03-03 03:30:40 +0100

    class and object

---
+++ src/com/matraore/Cat.java
@@ -0,0 +1,65 @@
+package com.matraore;
+
+import java.util.Objects;
+
+public class Cat {
+
+    private int age;
+    private String name;
+    private String color;
+
+    public Cat(int age, String name, String color) {
+        this.age = age;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Cat() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return age == cat.age && Objects.equals(name, cat.name) && Objects.equals(color, cat.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name, color);
+    }
+}
